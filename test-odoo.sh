b6c39f7f7e11aa313f7a29d1ad2bfdd303705885
marcuszxc <marcuszxc10@gmail.com>
2025-02-12 16:59:04 +0100

    New changes to test-odoo.sh T/0011

--- test-odoo.sh
+++ test-odoo.sh
@@ -5,6 +5,9 @@ ODOOREPO=""
 UBUNTUVERSION=""
 SHAREPATH="/usr/share"
 ODOOTOOLS="/etc/profile.d/odootools.sh"
+BREAK-SYSTEM-PACKAGES="[global]
+break-system-packages = true
+"
 
 usage() { echo "Usage: $0 [-v <odooversion>] [-r <odoorepo>] [-u <ubuntuversion]" 1>&2; exit 1;}
 
@@ -46,10 +49,30 @@ sudo lxc launch ubuntu:"$UBUNTUVERSION" "$MACHINENAME"
 echo "Waiting for the machine to receive an IP address..."
 sleep 5
 
+if [[ -z "$(sudo which pip)" ]]; then
+    echo No pip installd
+    echo Installing pip for root...
+    wget -O- https://bootstrap.pypa.io/get-pip.py | sudo python3
+fi
+
+if [ ! -d /root/.config ]; then
+    sudo lxc exec "$MACHINENAME" -- mkdir /root/.config
+fi
+
+if [ ! -d /root/.config/pip ]; then
+    sudo lxc exec "$MACHINENAME" -- mkdir /root/.config/pip
+fi
+
+if [ -e /root/.config/pip/pip.conf ]; then
+    sudo lxc exec "$MACHINENAME" -- bash -c "echo $BREAK-SYSTEM-PACKAGES >> pip.conf" 
+else
+    sudo lxc exec "$MACHINENAME" -- bash -c "echo $BREAK-SYSTEM-PACKAGES > pip.conf" 
+fi
+
 echo "Installing Odoo..."
 sudo lxc exec "$MACHINENAME" -- bash -c "wget -O- https://raw.githubusercontent.com/vertelab/odootools/18.0/install | bash" 
 
-if ! systemctl list-units --full -all | grep -Fq "$service_name.service"; then
+if ! systemctl list-units --full -all | grep -Fq "odoo.service"; then
     echo Odoo not installed succefuly
     exit 1 
 fi 
@@ -71,11 +94,11 @@ echo "Checking for odootools.sh..."
 if [ ! -e "$ODOOTOOLS" ]; then
     echo "No odootools.sh found"
     echo "Installing odootools.sh..."
-    sudo lxc exec "$MACHINENAME" -- bash -c "sudo wget -O /etc/profile.d/odootools.sh https://raw.githubusercontent.com/vertelab/odootools/common/odootools.sh"
+    sudo lxc exec "$MACHINENAME" -- bash -c "sudo wget -O $ODOOTOOLS https://raw.githubusercontent.com/vertelab/odootools/common/odootools.sh"
 fi
 
 echo "using odootools..."
-sudo lxc exec "$MACHINENAME" -- bash -c "source $ODOOTOOLS && odooaddons && odoosetperms"
+sudo lxc exec "$MACHINENAME" -- bash -c "source $ODOOTOOLS && odooaddons && odoosetperm && odoorestart"
 
 if [ -e "$ODOOEXTREQ" ]; then
     sudo lxc exec "$MACHINENAME" -- bash -c "source $ODOOTOOLS && odooreqclone"
