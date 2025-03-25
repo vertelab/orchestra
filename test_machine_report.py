2406950a53c1bfb4b9e20e1ee2449ebc8dd8a182
marcuszxc <marcuszxc10@gmail.com>
2025-03-25 14:13:19 +0100

    Refactord test_machine_report.py

--- test_machine_report.py
+++ test_machine_report.py
@@ -1,16 +1,15 @@
-import subprocess
-
 file_path = "/var/log/odoo/test-odoo-server.log"
 
-def exstract_tracebacks(log):
+def extract_tracebacks(split_log):
     tracebacks = []
     traceback = ""
     split_log = log.split("\n")
     check = False
     for line in split_log:
         if "INFO" in line or "WARNING" in line or "CRITICAL" in line:
-            if traceback:
+            if traceback and check:
                 tracebacks.append(traceback)
+                traceback=""
             check = False
         elif "Traceback" in line:
             check = True
@@ -18,13 +17,21 @@ def exstract_tracebacks(log):
             traceback += line
     return tracebacks
 
+def extract_on_message(split_log, extract_message):
+    messages = []
+    for line in split_log:
+        if extract_message in line:
+            messages.append(line)
+    return messages
+
 with open(file_path, "r", encoding="utf-8") as file:
 
     log = file.read()
-    warnings = subprocess.run(["grep", "WARNING", file_path], capture_output=True, text=True)
-    errors = subprocess.run(["grep", "ERROR", file_path], capture_output=True, text=True)
-    criticals = subprocess.run(["grep", "CRITICAL", file_path], capture_output=True, text=True)
-    tracebacks = exstract_tracebacks(log)
+    split_log = log.split("\n")
+    warnings = extract_on_message(split_log,"WARNING")
+    errors = extract_on_message(split_log, "ERROR")
+    criticals = extract_on_message(split_log, "CRITICAL")
+    tracebacks = extract_on_message(split_log, "Traceback")
     report = {"warnings":warnings, "errors": errors, "criticals": criticals, "tracebacks": tracebacks, "full_log": log}
     print(report)
     file.close()
