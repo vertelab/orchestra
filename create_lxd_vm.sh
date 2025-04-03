c3e601b6c61c6def80a46904a17b4fb5a8718bfd
marcuszxc <marcuszxc10@gmail.com>
2025-04-03 08:39:25 +0200

    create_lxd_vm.sh: Updated to now add the ip address to host and config file

--- create_lxd_vm.sh
+++ create_lxd_vm.sh
@@ -3,7 +3,8 @@
 read -rp "Enter name of the new machine: " NAME
 read -rp 'Enter ubuntu version: ' UBUNTUVERSION
 
-PUBKEY="$HOME/.ssh/id_rsa.pub"
+SSHDIR="$HOME/.ssh"
+PUBKEY="$SSHDIR/id_rsa.pub"
 
 sudo lxc launch ubuntu:"$UBUNTUVERSION" "$NAME" -d root,size=20GiB --vm
 
@@ -22,3 +23,12 @@ echo added .ssh dir
 sudo lxc file push "$PUBKEY" "$NAME"/home/"$USER"/.ssh/authorized_keys
 sudo lxc exec "$NAME" -- chown "$USER":"$USER" $HOME/.ssh/ -R
 echo added public key
+
+echo Adding ip address to host and config file 
+VMIP=$(sudo lxc exec "$NAME" -- ip a | grep -Po '\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}(?=\/24)')
+if [ ! -d "${SSHDIR}/config" ]; then
+    echo -e "\nHost $NAME\n  HostName $VMIP\n  ForwardAgent yes" > "$SSHDIR"/config
+else
+    echo -e "\nHost $NAME\n  HostName $VMIP\n  ForwardAgent yes" >> "$SSHDIR"/config
+fi
+echo -e "$VMIP $NAME" | sudo tee -a /etc/hosts
