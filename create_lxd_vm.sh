b9da81e414eacdcea88cfa46b9b53fd94c2d4704
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2025-05-08 12:32:17 +0200

    Update create_lxd_vm.sh for newer public keys

--- create_lxd_vm.sh
+++ create_lxd_vm.sh
@@ -4,28 +4,36 @@ read -rp "Enter name of the new machine: " NAME
 read -rp 'Enter ubuntu version: ' UBUNTUVERSION
 
 SSHDIR="$HOME/.ssh"
-PUBKEY="$SSHDIR/id_rsa.pub"
+PUBKEY=""
 
-sudo lxc launch ubuntu:"$UBUNTUVERSION" "$NAME" -d root,size=20GiB --vm
+if [ -f "$SSHDIR/id_rsa.pub" ]; then
+    PUBKEY="$SSHDIR/id_rsa.pub"
+elif [ -f "$SSHDIR/id_ed25519.pub" ]; then
+    PUBKEY="$SSHDIR/id_ed25519.pub"
+fi
+
+lxc launch ubuntu:"$UBUNTUVERSION" "$NAME" -d root,size=20GiB --vm
 
 echo waiting...
 sleep 30
 
-sudo lxc exec "$NAME" -- useradd -m -G sudo -s /bin/bash "$USER"
+lxc exec "$NAME" -- useradd -m -G sudo -s /bin/bash "$USER"
 echo added user
 
 echo "Please set a password for the user"
-sudo lxc exec "$NAME" -- passwd "$USER"
+lxc exec "$NAME" -- passwd "$USER"
 
-sudo lxc exec "$NAME" -- mkdir /home/"$USER"/.ssh
+lxc exec "$NAME" -- mkdir /home/"$USER"/.ssh
 echo added .ssh dir
 
-sudo lxc file push "$PUBKEY" "$NAME"/home/"$USER"/.ssh/authorized_keys
-sudo lxc exec "$NAME" -- chown "$USER":"$USER" $HOME/.ssh/ -R
-echo added public key
+if [ -n $PUBKEY ]; then
+    lxc file push "$PUBKEY" "$NAME"/home/"$USER"/.ssh/authorized_keys
+    lxc exec "$NAME" -- chown "$USER":"$USER" $HOME/.ssh/ -R
+    echo added public key
+fi
 
 echo Adding ip address to host and config file 
-VMIP=$(sudo lxc exec "$NAME" -- ip a | grep -Po '\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}(?=\/24)')
+VMIP=$(lxc exec "$NAME" -- ip a | grep -Po '\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}(?=\/24)')
 if [ ! -d "${SSHDIR}/config" ]; then
     echo -e "\nHost $NAME\n  HostName $VMIP\n  ForwardAgent yes" > "$SSHDIR"/config
 else
